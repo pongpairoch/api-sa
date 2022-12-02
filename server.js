e8c3c15d73bf00fe1a0f6c60d9b47507a56c4923
OAT <88110592+pongpairoch@users.noreply.github.com>
2022-12-02 20:17:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -207,25 +207,38 @@ const slot = {
 
 
 
-app2.get('/api/sa', (req,res) =>{
-	fetch('https://casinohacker.vip/salhacker/database/getresult.php', {
-  headers: {
-    Cookie: 'PHPSESSID=pmsn7dgrntcb6qc0b2jm9njrge'
-  }
-}).then(response =>  response.json())
-.then(data =>  res.json({"status":"200","data":[{"room_id":"1","room_name":"E01","records":data[0].records},
-{"room_id":"2","room_name":"E02","records":data[1].records},
-{"room_id":"3","room_name":"E03","records":data[2].records},
-{"room_id":"4","room_name":"E04","records":data[3].records},
-{"room_id":"5","room_name":"E05","records":data[4].records},
-{"room_id":"6","room_name":"E06","records":data[5].records},
-{"room_id":"7","room_name":"E07","records":data[6].records},
-{"room_id":"8","room_name":"E08","records":data[7].records},
-{"room_id":"9","room_name":"E09","records":data[8].records},
-{"room_id":"10","room_name":"E10","records":data[9].records}
 
-]}));
-})
+
+app2.get('/api/se', (req,res) =>{
+	fetch('https://pdmoney.net/api-bac/database/getlog_se.php', {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+}).then(response =>  response.json())
+.then(data =>  res.json({"status":"200","data":data.data}) )});
+// {"room_id":"2","room_name":"E02","records":data[1].records},
+// {"room_id":"3","room_name":"E03","records":data[2].records},
+// {"room_id":"4","room_name":"E04","records":data[3].records},
+// {"room_id":"5","room_name":"E05","records":data[4].records},
+// {"room_id":"6","room_name":"E06","records":data[5].records},
+// {"room_id":"7","room_name":"E07","records":data[6].records},
+// {"room_id":"8","room_name":"E08","records":data[7].records},
+// {"room_id":"9","room_name":"E09","records":data[8].records},
+// {"room_id":"10","room_name":"E10","records":data[9].records}
+// {"room_id":"11","room_name":"C01","records":data[10].records},
+// {"room_id":"12","room_name":"C02","records":data[11].records},
+// {"room_id":"13","room_name":"C03","records":data[12].records},
+// {"room_id":"14","room_name":"C04","records":data[13].records},
+// {"room_id":"15","room_name":"C05","records":data[14].records},
+// {"room_id":"16","room_name":"C06","records":data[15].records},
+// {"room_id":"17","room_name":"C07","records":data[16].records},
+// {"room_id":"18","room_name":"E08","records":data[17].records},
+// {"room_id":"19","room_name":"E09","records":data[18].records},
+// {"room_id":"20","room_name":"E10","records":data[19].records},
+// {"room_id":"21","room_name":"E10","records":data[20].records},
+// {"room_id":"22","room_name":"E10","records":data[21].records},
+
+
+
+
 
 
 
